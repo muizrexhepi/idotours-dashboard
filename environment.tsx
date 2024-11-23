1c0a1e017e1d0f81048ac106c9c93b96e65d09f3
etnik2002 <etnikz2002@gmail.com>
2024-11-23 20:32:24 +0100

    live chat

--- environment.tsx
+++ environment.tsx
@@ -1,4 +1,8 @@
 // export const API_URL = "http://localhost:1234";
 export const API_URL = "https://api.gobusly.com";
-export const SUPPORT_URL = "https://mebus-website.vercel.app/help/contact-support";
-export const HELP_URL = "https://mebus-website.vercel.app/help";
+export const SUPPORT_URL = "https://www.gobusly.com/help/help/contact-support"
+export const HELP_URL = "https://www.gobusly.com/help"
+
+// export const SOCKET_URL = 'http://localhost:3030';
+export const SOCKET_URL = "https://gobusly-livechat.onrender.com";
+export const GOBUSLY_SUPPORT_USER_ID = "6738ebf98fc24f8f9b724a05";
